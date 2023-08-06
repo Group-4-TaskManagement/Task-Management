2f64ecc2ab8379a0ad2ba029ee972d272e8f6013
Marian <marianmgm@yahoo.com>
2023-08-06 19:38:40 +0300

    add utils

---
+++ TaskManagement/src/utils/FormattingHelpers.java
@@ -0,0 +1,10 @@
+package utils;
+
+import java.math.BigDecimal;
+
+public class FormattingHelpers {
+    public static String removeTrailingZerosFromDouble(double number) {
+        BigDecimal num = BigDecimal.valueOf(number).stripTrailingZeros();
+        return num.toPlainString();
+    }
+}
